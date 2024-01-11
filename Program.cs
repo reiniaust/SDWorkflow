f27365c8556127a45e0b6f414c212a375c480d14
reiniaust <55030087+reiniaust@users.noreply.github.com>
2024-01-11 17:46:58 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,67 +2,94 @@ using Newtonsoft.Json;
 using System;
 
 MyClass currentItem;
+MyClass newItem;
+MyClass modifyItem;
 MyClass cutItem;
 List<MyClass> currentList;
 string json;
 List<MyClass> data;
 string? input = "r";
+string filePath = @"S:\SDWorkflow\data.json";
 
 while (input == "r")
 {
-    json = File.ReadAllText(@"S:\SDWorkflow\data.json");
-    data = JsonConvert.DeserializeObject<List<MyClass>>(json);
+    try
+    {
+        json = File.ReadAllText(filePath);
+        data = JsonConvert.DeserializeObject<List<MyClass>>(json);
+    }
+    catch (System.Exception)
+    {
+        data = new();
+        data.Add(new MyClass() { Id = 1, Name = "SDWorkflow" });
+    }
 
     currentItem = data[0];
-    currentList = data.Where(item => item.ParentId== 1).ToList();
+    currentList = data.Where(item => item.ParentId == 1).ToList();
+    newItem = null;
+    modifyItem = null;
     cutItem = null;
 
     showList();
-    
-    saveData(); 
-}
 
+    saveData();
+}
 
 void saveData()
 {
     json = JsonConvert.SerializeObject(data, Formatting.Indented);
-    File.WriteAllText(@"S:\SDWorkflow\data.json", json);
+    File.WriteAllText(filePath, json);
 }
-
 
 void showList()
 {
     Console.Clear();
-    Console.WriteLine(currentItem.Name+ " (" + currentItem.TimeStamp + ")");
+    Console.WriteLine(currentItem.Name + " (" + currentItem.TimeStamp + ")");
     Console.WriteLine();
     int i = 0;
     foreach (MyClass item in currentList.OrderBy(i => i.TimeStamp).ToList())
     {
         i += 1;
         item.Position = i;
-        Console.WriteLine(item.Position + " " + item.Name);
+        string plus = "";
+        if (data.Where(child => child.ParentId == item.Id).ToList().Count > 0)
+        {
+            plus = " +";
+        }
+        Console.WriteLine(item.Position + " " + item.Name + plus);
     }
 
     Console.WriteLine();
+    Console.WriteLine("h Hinzufügen");
     if (currentItem.Id != 1)
     {
-        Console.WriteLine("r Refresh");
-        Console.WriteLine("z Zurück");
-        if (cutItem is null)
-        {
-            Console.WriteLine("x Ausschneiden");
-        }
+        Console.WriteLine("ä Ändern");
+        Console.WriteLine("l Löschen/Ausschneiden");
     }
     if (cutItem is not null)
     {
-        Console.WriteLine("v Einfügen");
+        Console.WriteLine("e Einfügen");
     }
-    Console.WriteLine("e Ende");
+    if (currentItem.Id != 1)
+    {
+        Console.WriteLine("z Zurück");
+    }
+    if (currentItem.Id != 1 || cutItem is not null)
+    {
+        Console.WriteLine("r Refresh");
+    }
+    Console.WriteLine("x Ende");
     Console.WriteLine();
+
+    if (input == "h" || input == "ä")
+    {
+        // Ändern
+        Console.WriteLine("Neuer Titel:");
+    }
 
     input = Console.ReadLine();
 
-    if (input != "e" && input != "r")
+    if (input != "x" && input != "r")
     {
         if (input == "z" && currentItem.ParentId != 0)
         {
@@ -70,64 +97,102 @@ void showList()
         }
         else
         {
-            if (input == "x")
+            if (newItem is not null)
             {
-                cutItem = currentItem;
-                currentItem = data.Find(item => item.Id == currentItem.ParentId);
-                cutItem.ParentId = 0;
+                // Text hinzufügen und speichern
+                newItem = new()
+                {
+                    Id = data.Max(item => item.Id) + 1,
+                    Name = input,
+                    ParentId = currentItem.Id,
+                    TimeStamp = DateTime.Now
+                };
+                data.Add(newItem);
+                saveData();
+                newItem = null;
             }
             else
             {
-                if (input == "v")
+
+                if (modifyItem is not null)
                 {
-                    cutItem.ParentId = currentItem.Id;
-                    cutItem.TimeStamp = DateTime.Now;
-                    cutItem = null;
+                    // Änderung speichern
+                    modifyItem.Name = input;
                     saveData();
+                    modifyItem = null;
                 }
                 else
                 {
-                    bool found = false;
-                    foreach (MyClass item in currentList)
+                    if (input == "h")
                     {
-                        if (input == item.Position.ToString() || item.Name.ToLower().Contains(input.ToLower()))
-                        {
-                            currentItem = item;
-                            found = true;
-                            break;
-                        }
+                        // Hinzufügen
+                        newItem = new();
                     }
-                    if (!found)
-                    {
-                        MyClass foundItem = FoundChildItem(currentList, input);
-
-                        if (foundItem is null)
+                    else
                         {
-                            currentItem = new()
-                            {
-                                Id = data.Max(item => item.Id) + 1,
-                                Name = input,
-                                ParentId = currentItem.Id,
-                                TimeStamp = DateTime.Now
-                            };
-                            data.Add(currentItem);
-                            saveData();
+                        if (input == "ä")
+                        {
+                            // Ändern
+                            modifyItem = currentItem;
                         }
                         else
                         {
-                            currentItem= foundItem;
+                            if (input == "l")
+                            {
+                                // Löschen/Ausschneiden
+                                cutItem = currentItem;
+                                currentItem = data.Find(item => item.Id == currentItem.ParentId);
+                                cutItem.ParentId = 0;
+                            }
+                            else
+                            {
+                                if (input == "e")
+                                {
+                                    // Einfügen
+                                    cutItem.ParentId = currentItem.Id;
+                                    cutItem.TimeStamp = DateTime.Now;
+                                    cutItem = null;
+                                    saveData();
+                                }
+                                else
+                                {
+                                    bool found = false;
+                                    foreach (MyClass item in currentList)
+                                    {
+                                        if (input == item.Position.ToString() || item.Name.ToLower().Contains(input.ToLower()))
+                                        {
+                                            currentItem = item;
+                                            found = true;
+                                            break;
+                                        }
+                                    }
+                                    if (!found)
+                                    {
+                                        MyClass foundItem = FoundChildItem(currentList, input);
+
+                                        // Neuen Punkt anlegen/hinzufügen
+                                        if (foundItem is not null)
+                                            currentItem = foundItem;
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
-            }
         }
-        currentList = data.Where(item => item.ParentId == currentItem.Id).ToList();
+        if (currentItem != null)
+        {
+            currentList = data.Where(item => item.ParentId == currentItem.Id).ToList();
+        }
         showList();
     }
 
-    MyClass FoundChildItem(List<MyClass> list, string search) {
+    MyClass FoundChildItem(List<MyClass> list, string search)
+    {
         MyClass item = null;
-        foreach (MyClass child in list) {
+        foreach (MyClass child in list)
+        {
             if (child.Name.ToLower().Contains(search.ToLower()))
             {
                 item = child;
@@ -167,4 +232,5 @@ class MyClass
     public int Position { get; set; }
     public DateTime TimeStamp { get; set; } = DateTime.Now;
 }
+
 
